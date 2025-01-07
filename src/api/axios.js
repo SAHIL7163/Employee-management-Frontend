5d421b4930c1b4add329251ed8ceafe19d662b36
SAHIL7163 <137681165+SAHIL7163@users.noreply.github.com>
2025-01-07 21:40:38 +0530

    Update axios.js

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
-const BASE_URL = 'http://localhost:3500';
+/* const BASE_URL = 'http://localhost:3500'; */
+const BASE_URL = 'https://employee-management-backend-0xef.onrender.com';
 
 export default axios.create(
     {
